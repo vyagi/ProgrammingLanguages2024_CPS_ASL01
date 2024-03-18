81c239346dc2d5ab77b5a79ac482777ed5dc42db
vyagi <marcin.jagiela@hotmail.com>
2024-03-18 18:04:22 +0100

    Done

--- ClassesProjects/LinqTutorial/Program.cs
+++ ClassesProjects/LinqTutorial/Program.cs
@@ -1,39 +1,61 @@
-﻿using Geometry;
+﻿List<int> numbers1 = new List<int> { 2, 5, 10, 12, 5, 1, 12, 2, 10, 11, 4 };
 
-int[] numbers1 = { 2, 5, 10, 12, 5, 1, 12, 2, 10, 11, 4 };
-int[] numbers2 = { 10, 1, 56, 7, 12, 1, 8, 5 , 1, 2, 2 };
+var bigNumbers = numbers1.Where(x => x > 10).ToArray();
 
-//Console.WriteLine(numbers1.Sum());
-//Console.WriteLine(numbers1.Count());
-//Console.WriteLine(numbers1.Min());
-//Console.WriteLine(numbers1.Max());
-//Console.WriteLine(numbers1.Average());
+numbers1.Add(50);
 
-//var z = numbers1.Zip(numbers2);
-//var z = numbers1.Union(numbers2);
-//var z = numbers1.Intersect(numbers2);
-//var z = numbers1.Concat(numbers2);
-//var z = numbers1.Except(numbers2);
-
-//var z = numbers1.Union(numbers2).Except(numbers1.Intersect(numbers2));
-var z = numbers1.Except(numbers2).Union(numbers2.Except(numbers1));
-
-foreach (var item in z)
+foreach (var item in bigNumbers)
 {
     Console.WriteLine(item);
 }
 
 
 
-//var pc = new PolygonalChain(new Point(1, 1), new Point(5, 8));
-//pc.AddMidpoint(new Point(1, 2));
-//pc.AddMidpoint(new Point(1, 3));
-//pc.AddMidpoint(new Point(1, 4));
-//pc.AddMidpoint(new Point(1, 5));
+var persons = new Person[] {
+    new Person(1, "James", "Bond", 40, new [] { "Spying", "Women", "Cars" }),
+    new Person(2, "Kirk", "Hammett", 50, new [] { "Guitar", "Comic books", "Horrors" }),
+    new Person(3, "Ozzy", "Ozbourne", 70, new [] { "Guitar", "Singing", "Women", "Alcohol", "Drugs" }),
+    new Person(4, "John", "Lennon", 66, new [] { "Guitar", "Singing", "Poetry" }),
+    new Person(5, "Donald", "Trump", 80, new [] { "Money", "Women", "Power" }),
+    new Person(6, "Donald", "Duck", 71, new [] { "Seed", "Quacking" }),
+    new Person(7, "John", "Travolta", 33, new [] { "Singing", "Acting" }),
+};
+var addresses = new Address[] {
+    new Address(1, 1, "London", "Baker street", TypeOfAddress.Home),
+    new Address(2, 1, "Birmingham", "Aldrige St.", TypeOfAddress.Mail),
+    new Address(3, 2, "San Francisco", "10 Avenue", TypeOfAddress.Home),
+    new Address(4, 2, "San Francisco", "Roman Blvd", TypeOfAddress.Mail),
+    new Address(5, 2, "Los Angeles", "Mulholland Drive", TypeOfAddress.Temporary),
+    new Address(6, 2, "Los Angeles", "Beverly Hills 90210", TypeOfAddress.Temporary),
+    new Address(100, -1, "Moon", "Dark side", TypeOfAddress.Home),
+    new Address(100, -1, "Sun", "Gas", TypeOfAddress.Temporary),
+};
 
-////foreach(var point in pc)
-////    Console.WriteLine(point);
+//var byHobbies = persons.GroupBy(x => x.Hobbies.Length).OrderBy(x=>x.Key);
 
-//Console.WriteLine(pc.First());
-//Console.WriteLine(pc.Last());
+//foreach (var oneGroup in byHobbies)
+//{
+//    Console.WriteLine(oneGroup.Key);
+//    foreach (var item in oneGroup)
+//    {
+//        Console.WriteLine(item.FirstName + " " + item.LastName);
+//    }
+//}
 
+//var byAgeRanges = persons.GroupBy(x => x.Age / 10);
+
+//foreach (var oneGroup in byAgeRanges)
+//{
+//    Console.WriteLine(oneGroup.Key * 10);
+//    foreach (var item in oneGroup)
+//    {
+//        Console.WriteLine(item.FirstName + " " + item.LastName);
+//    }
+//}
+
+
+enum TypeOfAddress { Home, Mail, Temporary }
+record Person(int Id, string FirstName, string LastName, int Age, string[] Hobbies);
+record Address(int Id, int PersonId, string City, string Street, TypeOfAddress AddressType);
+record Book(int Id, string Title);
+record Author(int Id, string Name);
---
+++ ClassesProjects/LinqTutorial/Program1.cs
@@ -0,0 +1,70 @@
+﻿//using Geometry;
+//using System.Globalization;
+
+//int[] numbers1 = { 2, 5, 10, 12, 5, 1, 12, 2, 10, 11, 4 };
+//int[] numbers2 = { 10, 1, 56, 7, 12, 1, 8, 5 , 1, 2, 2 };
+
+#region Done
+//Console.WriteLine(numbers1.Sum());
+//Console.WriteLine(numbers1.Count());
+//Console.WriteLine(numbers1.Min());
+//Console.WriteLine(numbers1.Max());
+//Console.WriteLine(numbers1.Average());
+
+//var z = numbers1.Zip(numbers2);
+//var z = numbers1.Union(numbers2);
+//var z = numbers1.Intersect(numbers2);
+//var z = numbers1.Concat(numbers2);
+//var z = numbers1.Except(numbers2);
+
+//var z = numbers1.Union(numbers2).Except(numbers1.Intersect(numbers2));
+//var z = numbers1.Except(numbers2).Union(numbers2.Except(numbers1));
+//var z = numbers1.Skip(3).Take(3);
+
+//var z = numbers1.TakeWhile(x => x < 11);
+//var z = numbers1.SkipWhile(x => x < 11);
+
+//var z = numbers1.TakeLast(4);
+//var z = numbers1.SkipLast(4);
+
+//foreach (var item in z)
+//{
+//    Console.WriteLine(item);
+//}
+
+#endregion Done
+
+//var chunks = numbers1.Chunk(3);
+//foreach (var chunk in chunks)
+//{
+//	Console.WriteLine("Next chunk");
+//	foreach (var element in chunk)
+//	{
+//		Console.WriteLine(element);
+//	}
+//}
+
+//Console.WriteLine(numbers1.SequenceEqual(numbers2));
+//Console.WriteLine(numbers1.SequenceEqual(numbers1));
+
+//var y = numbers1.All(x => x > 0);
+//var y = numbers1.Any(x => x > 50);
+//Console.WriteLine(y);
+
+//string[] names = { "James", "Bob", "Sam", "Keanu", "Adam" };
+//Console.WriteLine(names.OrderByDescending(x => x.Length).First().Length);
+//Console.WriteLine(names.MaxBy(x=>x.Length).Length);
+
+
+//var pc = new PolygonalChain(new Point(1, 1), new Point(5, 8));
+//pc.AddMidpoint(new Point(1, 2));
+//pc.AddMidpoint(new Point(1, 3));
+//pc.AddMidpoint(new Point(1, 4));
+//pc.AddMidpoint(new Point(1, 5));
+
+////foreach(var point in pc)
+////    Console.WriteLine(point);
+
+//Console.WriteLine(pc.First());
+//Console.WriteLine(pc.Last());
+
